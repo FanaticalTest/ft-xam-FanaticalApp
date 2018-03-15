b6b12d8c518faaad8de29f8c826658c4f5f1e2e9
Ziridis <samy@kacem.net>
2018-03-15 11:28:46 +0100

    Initial load

---
+++ FanaticalApp/MainNavigation.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace FanaticalApp
+{
+    public partial class MainNavigation : TabbedPage
+    {
+        public MainNavigation()
+        {
+            InitializeComponent();
+        }
+    }
+}
